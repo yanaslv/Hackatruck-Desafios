bba153f9b1ccc13bf385f76bc28d4f980c7d9dc1
Yanne A. Alves <163936951+yanaslv@users.noreply.github.com>
2025-03-27 13:09:45 -0300

    Add files via upload

---
+++ Aulas/Aula1/Aula1/Aula1App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula1App.swift
+//  Aula1
+//
+//  Created by Turma01-28 on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Aula1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
